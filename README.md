68e12e5041670307a069a5a203dd7446cf04b232
Thibault Lacroux <thibault.lacroux@gmail.com>
2017-04-05 16:18:33 +0200

    Adding configuration options, deps management & updating README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # linter-elixir-dogma
+
+Atom plugin to check Elixir code with Dogma
+
+## Requirements
+
+You may need to have Dogma installed globaly :
+
+```bash
+cd /tmp
+git clone https://github.com/lpil/dogma
+cd dogma
+MIX_ENV=prod mix do deps.get, archive.build, archive.install
+```
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import { CompositeDisposable } from 'atom'
 import * as helpers from 'atom-linter'
 import { dirname } from 'path'
+import { install as installDeps } from 'atom-package-deps'
 
 const regex = /(\d+): (.+)/gm
 
@@ -31,3 +32,15 @@ export const provideLinter = () => ({
     return messages
   },
 })
+
+export const activate = () =>
+  installDeps('linter')
+
+export const config = {
+  executablePath: {
+    type: 'string',
+    default: 'mix',
+    description: 'Absolute path to the mix executable on your system.' +
+      'Example: /usr/local/bin/mix , by default it checks for mix in your path'
+  }
+}
